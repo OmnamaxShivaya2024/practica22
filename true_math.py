e5f5ad10299dd0b24efa2b6b0940debb33aedcfa
Людмила Кузнецова <ananim2023@list.ru>
2024-09-14 14:26:16 +0300

    Add files via upload

---
+++ true_math.py
@@ -0,0 +1,13 @@
+def divide(first , second):
+    if second !=0:
+        result = first/second
+        return result
+    if second==0:
+        from math import inf
+        return inf
+    
+
+
+
+
+
